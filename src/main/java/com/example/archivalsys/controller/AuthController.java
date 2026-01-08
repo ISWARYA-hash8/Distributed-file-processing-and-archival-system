ebc4fbe3d7bfb26eaaa7ea2a8f08e9ad3014c862
Iswarya P <iswarya83p@gmail.com>
2026-01-08 09:09:23 +0530

    Added CrossOrigin to overcome CORS error

--- src/main/java/com/example/archivalsys/controller/AuthController.java
+++ src/main/java/com/example/archivalsys/controller/AuthController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+    @CrossOrigin(
+        origins = "${frontend.url}",
+        allowedHeaders = "*",
+        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE},
+        allowCredentials = "true"
+)
 public class AuthController {
 
     private final AuthService authService;
